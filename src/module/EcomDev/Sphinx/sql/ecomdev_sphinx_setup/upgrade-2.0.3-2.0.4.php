5a28ae2dcd833ca194785fe8b2835330730dee5e
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2015-12-12 15:16:57 +0000

    Fix sphinx_scope attribute

---
+++ src/module/EcomDev/Sphinx/sql/ecomdev_sphinx_setup/upgrade-2.0.3-2.0.4.php
@@ -0,0 +1,17 @@
+<?php
+
+$catalogInstaller = new Mage_Catalog_Model_Resource_Setup('core_setup');
+$catalogInstaller->addAttribute(
+    Mage_Catalog_Model_Category::ENTITY,
+    'sphinx_scope',
+    array(
+        'type'                       => 'int',
+        'label'                      => 'Sphinx Scope',
+        'input'                      => 'select',
+        'source'                     => 'ecomdev_sphinx/source_catalog_scope',
+        'sort_order'                 => 60,
+        'global'                     => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+        'group'                      => 'General',
+        'required'                   => false
+    )
+);
